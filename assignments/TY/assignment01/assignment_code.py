8979afc354d985464600624feaca23b8350ca03b
tanisha0523 <tanisha0523@gmail.com>
2025-02-02 18:52:07 -0800

    BY assignment

--- assignments/TY/assignment01/assignment_code.py
+++ assignments/TY/assignment01/assignment_code.py
@@ -67,8 +67,6 @@ concert_info = {
                     ],
                 }
 
-
-
 def convert_realInfo_into_str(info):
     """convert input information into string type value
         this function is desinged to save the list or dict type of data into dataframe
@@ -109,9 +107,19 @@ class ConcertInfoController():
         1. convert list type of bands information into dataframe type of bands information
         2. and then save it in excel file or other file format
         """
-        pass
+        created_table = create_table(self.bands)
+        print(created_table)
+        created_table.to_excel(excel_writer="C:/dev/test.xlsx", sheet_name="BY")
+        created_table.to_json("C:/dev/test.json")
 
 
+controller = ConcertInfoController(
+    concert_info["name"],
+    concert_info["location"],
+    concert_info["date"],
+    concert_info["bands"],
+)
+controller.save_information_into_database()
 
 
 
@@ -120,26 +128,31 @@ class ConcertInfoController():
 # This is an example regarding how to use convert list or
 # dict type of information into string type value
 # =======================================================
-real_info = [
-                {
-                    "title": "Satisfaction",
-                    "duration": 233,
-                },
-                {
-                    "title": "Gimme Shelter",
-                    "duration": 272,
-                },
-                {
-                    "title": "Jumpin' Jack Flash",
-                    "duration": 220,
-                },
-            ]
+# real_info = [
+#                 {
+#                     "title": "Satisfaction",
+#                     "duration": 233,
+#                 },
+#                 {
+#                     "title": "Gimme Shelter",
+#                     "duration": 272,
+#                 },
+#                 {
+#                     "title": "Jumpin' Jack Flash",
+#                     "duration": 220,
+#                 },
+#             ]
 
 
-converted_info = convert_realInfo_into_str(real_info)
-print(converted_info)
-print(type(converted_info))
+# converted_info = convert_realInfo_into_str(real_info)
+# print(converted_info)
+# print(type(converted_info))
 
-real_info_from_str = convert_str_into_realInfo(converted_info)
-print(real_info_from_str)
-print(type(real_info_from_str))
+# real_info_from_str = convert_str_into_realInfo(converted_info)
+# print(real_info_from_str)
+# print(type(real_info_from_str))
+
+
+# todo list
+# 1. 컨트롤러로 bands 내용만 따로 빼기
+# 2. 엑셀에 저장장
