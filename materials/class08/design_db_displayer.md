9f9a2680a52128ee0be783b01696a28bc6b3b3f8
taiyeong.song <runabird36@gmail.com>
2025-02-16 20:27:09 -0800

    Add : design document and image

---
+++ materials/class08/design_db_displayer.md
@@ -0,0 +1,11 @@
+# Tool name
+- Database displayer
+
+# Features of Tool
+- 데이터베이스 정보 추출
+    - 데이터베이스가 저장되어있는데, 툴이 show 되었을때, 데이터 베이스 내용을 display 시키기
+- 원하는 정보 검색
+    - 검색기능 : 사용자의 각 cell의 값에 따라서 그 cell이 포함된 row 출력하는 기능
+<!-- - 데이터베이스 수정 및 보완
+    - 사용자 input을 입력받아 database에 반영 -->
+![tool_image](./resources/tool_design.png)
--- materials/class08/pandas_read.py
+++ materials/class08/pandas_read.py
@@ -1,34 +1,15 @@
-# Pandas definition : python Module for Database
-# Database :
-#           - backend
-#           - the feature or structure for users to store data
-#
-# Two categories of Database
-#   - a relational database management system   : ex - MySQL, pandas
-#   - a NoSQL database system                   : ex - MongoDB
-#
-# api = group of functions or classes
-#   ex) import pandas
-#   ex) import ...
-#
-# CRUD for api
-#   - C : Create api
-#   - R : Read api
-#   - U : Update api
-#   - D : Delete api
-from pandas import DataFrame
+import pandas as pd
 
 # ===========
 # Create
 # ===========
-import pandas as pd
 
 def create_table(default_info :dict) -> pd.DataFrame:
     return pd.DataFrame(default_info)
 
 default_header = {"task_name":["a", "b"], "task_id":[1,2], "assignee":["ty", "by"], "entity_id":[111, 222]}
 table = create_table(default_header)
-# print(table)
+
 
 # ===========
 # Update
@@ -37,9 +18,6 @@ def add_row_to_table(tar_table :pd.DataFrame, input_info :dict) -> None:
     tar_table.loc[len(tar_table)] = input_info
 
 
-# input_data = {"task_name":"c", "task_id":3, "assignee":"dh", "entity_id":333}
-# add_row_to_table(table, input_data)
-# print(table)
 
 
 
@@ -57,10 +35,27 @@ print(table)
 # table.to_json("C:/dev/test.json")
 
 
-
+# =======================================================
+# Read one row which has keyword that user want to put
+# =======================================================
 def search_in_table(tar_table :pd.DataFrame, col_name :str, input_info :str) -> pd.DataFrame:
     return tar_table.loc[tar_table[col_name] == input_info]
 
 
 selected_row = search_in_table(table, "task_name", "fx01")
 print(selected_row)
+
+# =======================================================
+# Read one cell
+# =======================================================
+# print(table.iloc[0]["task_name"])
+
+
+
+
+# =======================================================
+# Read all row using for-loop
+# =======================================================
+for _idx, _row in table.iterrows():
+    # print(_row)
+    print(_idx, _row["task_id"])
